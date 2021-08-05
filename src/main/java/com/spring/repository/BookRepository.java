4d8be6264325d5dcfd05409b931746a761469370
Nikita <xmil.nik@gmail.com>
2021-08-06 00:10:19 +0300

    Adding BookRepository

---
+++ src/main/java/com/spring/repository/BookRepository.java
@@ -0,0 +1,22 @@
+package com.spring.repository;
+
+import com.spring.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book,Integer> {
+   @Query (value = "SELECT b FROM Book b WHERE b.bookAuthor LIKE concat('%', :keyword, '%')"
+            + " OR b.bookTitle LIKE concat('%', :keyword, '%')")
+    public List<Book> search(@Param("keyword") String keyword);
+
+
+   @Query (value = "select sum(b.price*b.count) from Book b")
+    Integer sumOfPrice();
+
+    @Query (value = "select sum (b.count) from Book b")
+    Integer booksCount();
+
+}
