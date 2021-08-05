0dd3fd9b4cf7f3cd6b342a7d6d4ed2d51f836a22
Nikita <xmil.nik@gmail.com>
2021-08-06 00:18:13 +0300

    Adding BookController

---
+++ src/main/java/com/spring/controllers/BookController.java
@@ -0,0 +1,67 @@
+package com.spring.controllers;
+
+import com.spring.models.Book;
+import com.spring.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @RequestMapping("/")
+    public ModelAndView home() {
+        List<Book> bookList = bookService.listAll();
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("bookList", bookList);
+        mav.addObject("sum",bookService.sumOfPrice());
+        mav.addObject("count",bookService.countOfBooks());
+        return mav;
+    }
+
+
+    @RequestMapping("/new")
+    public String newBookForm(Map<String, Object> model) {
+        Book book = new Book();
+        model.put("book", book);
+        return "new_book";
+    }
+    @RequestMapping (value = "/save", method = RequestMethod.POST)
+    public String saveBook(@ModelAttribute("book") Book book) {
+        bookService.save(book);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit")
+    public ModelAndView editBookForm(@RequestParam int id) {
+        ModelAndView mav = new ModelAndView("edit_book");
+        Book book = bookService.get(id);
+        mav.addObject("book", book);
+        return mav;
+    }
+    @RequestMapping("/delete")
+    public String deleteBookForm(@RequestParam int id) {
+        bookService.delete(id);
+        return "redirect:/";
+    }
+    @RequestMapping("/search")
+    public ModelAndView search(@RequestParam String keyword) {
+        List<Book> result = bookService.search(keyword);
+        ModelAndView mav = new ModelAndView("search");
+        mav.addObject("result", result);
+        return mav;
+    }
+
+
+}
