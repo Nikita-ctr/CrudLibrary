68b908db1df884c018c025a1966c964ad03071f3
Nikita <xmil.nik@gmail.com>
2021-08-06 00:13:29 +0300

    Adding BookService

---
+++ src/main/java/com/spring/service/BookService.java
@@ -0,0 +1,50 @@
+package com.spring.service;
+
+import com.spring.models.Book;
+import com.spring.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class BookService {
+
+    private final BookRepository bookRepository;
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    public List<Book> listAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book get(int id) {
+        return bookRepository.getOne(id);
+    }
+
+    public void delete(int id) {
+        bookRepository.deleteById(id);
+    }
+
+    public List<Book> search(String keyword) {
+        return bookRepository.search(keyword);
+    }
+
+    public Integer sumOfPrice(){
+        return bookRepository.sumOfPrice();
+    }
+    public Integer countOfBooks(){
+        return bookRepository.booksCount();
+    }
+
+
+}
